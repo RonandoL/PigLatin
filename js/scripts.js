629bf1664812e9d43f906dbc1ffccaf787dc66fa
Dustin Davis and Ronando Long <ronando.long@gmail.com>
2016-01-20 13:54:49 -0800

    It will slice a word that begins with a consonant and glue it on the end of the word and add ay

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,18 @@ var pigLatin = function(englishWord) {
   var vowels = ["a", "e", "i", "o", "u"];
 
   for (var i = 0; i < vowels.length; i++) {
-    var vowel = vowels[i];
-   if (englishWord[0] === vowel) {
-     return englishWord + "ay";  // add 'ay' if starts with vowel
-   } else {
-     return englishWord.slice(1) + englishWord[0] + "ay"  // slice first consanent (which is not a vowel) move to end add 'ay'
+     if (englishWord[0] === vowels[i]) {
+     var startsWithVowel = true;
+     break;
    }
-
   }
 
+   if (startsWithVowel) {
+     var translatedWord = englishWord; // it starts with vowel
+   } else {
+     var translatedWord = englishWord.slice(1) + englishWord[0];   // this is a consonant, slice first consonant (which is not a vowel) move to end add 'ay'
+   }
+   return translatedWord + "ay";  // add 'ay' if starts with vowel
 };
 
 
